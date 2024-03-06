24cd7b8d1b92b5dc09838d9f5afb3f7e6adb5190
caioe <caioevandro0@gmail.com>
2024-03-06 16:24:36 -0300

    Doughnut add

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,6 +1,7 @@
 import styles from "./page.module.css";
 import Cabecalho from "./components/Header/Header";
 import Chart from "./components/chart/Chart";
+import Circulo from "./components/chart/Doughnut";
 //https://power-bi-lilac.vercel.app
 
 
@@ -11,6 +12,7 @@ export default function Home() {
     <div className={styles.main}>
       <Cabecalho />
       <Chart />
+      <Circulo />
     </div>
   );
 }
